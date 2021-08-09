4dd1bc207cc39b29005e29ecd4e9f7a97bb1ac98
dngwoodo <dngwoodo@gmail.com>
2021-08-09 23:40:02 +0900

    feat: Add loading, error processing
    thunk 함수를 처리할 때 pending, fulfilled, rejected 별로 상태 변경하는 로직 추가

--- redux/slice.ts
+++ redux/slice.ts
@@ -5,21 +5,24 @@ import { Basic, Career, CareerInputName } from '@/types/Resume';
 
 import { loadResume, createCareer, deleteCareer, changeResume } from './thunks';
 
-type ResumeState = {
+export type ResumeState = {
   title: string;
   basic: Basic;
   careers: Career[];
-  loadResumeLoading: boolean;
-  loadResumeError: any;
-  createCareerLoading: boolean;
-  createCareerError: any;
-  deleteCareerLoading: boolean;
-  deleteCareerError: any;
-  changeResumeLoading: boolean;
-  changeResumeError: any;
 };
 
-export const initialState: ResumeState = {
+type InitialState = {
+  loadResumeLoading: boolean;
+  loadResumeError?: string;
+  createCareerLoading: boolean;
+  createCareerError?: string;
+  deleteCareerLoading: boolean;
+  deleteCareerError?: string;
+  changeResumeLoading: boolean;
+  changeResumeError?: string;
+};
+
+export const initialState: ResumeState & InitialState = {
   title: '이력서 제목',
   basic: {
     name: '',
@@ -31,13 +34,13 @@ export const initialState: ResumeState = {
   },
   careers: [],
   loadResumeLoading: false,
-  loadResumeError: null,
+  loadResumeError: undefined,
   createCareerLoading: false,
-  createCareerError: null,
+  createCareerError: undefined,
   deleteCareerLoading: false,
-  deleteCareerError: null,
+  deleteCareerError: undefined,
   changeResumeLoading: false,
-  changeResumeError: null,
+  changeResumeError: undefined,
 };
 
 export const { reducer, actions } = createSlice({
@@ -79,7 +82,6 @@ export const { reducer, actions } = createSlice({
         value: string;
       }>
     ) {
-      // eslint-disable-next-line no-param-reassign
       state.careers.find((career) => career.id === id)![name] = value;
     },
   },
@@ -88,58 +90,74 @@ export const { reducer, actions } = createSlice({
       // changeResume
       .addCase(changeResume.pending, (state) => {
         state.changeResumeLoading = true;
-        state.changeResumeError = null;
+        state.changeResumeError = undefined;
       })
-      .addCase(changeResume.fulfilled, (state, action) => {
+      .addCase(changeResume.fulfilled, (state, { payload: newResumeState }) => {
         state.changeResumeLoading = false;
-        state = action.payload;
-        state.changeResumeError = null;
+        state = { ...state, ...newResumeState };
+        state.changeResumeError = undefined;
       })
       .addCase(changeResume.rejected, (state, action) => {
         state.changeResumeLoading = false;
-        state.changeResumeError = action.payload;
+        if (action.payload) {
+          state.changeResumeError = action.payload.errorMessage;
+          return;
+        }
+        state.changeResumeError = action.error.message;
       })
       // loadResume
       .addCase(loadResume.pending, (state) => {
         state.loadResumeLoading = true;
-        state.loadResumeError = null;
+        state.loadResumeError = undefined;
       })
       .addCase(loadResume.fulfilled, (state, action) => {
         state.loadResumeLoading = false;
-        state = action.payload;
-        // state.loadResumeError = null;
+        state = { ...state, ...action.payload };
+        state.loadResumeError = undefined;
       })
       .addCase(loadResume.rejected, (state, action) => {
         state.loadResumeLoading = false;
-        state.loadResumeError = action.payload;
+        if (action.payload) {
+          state.loadResumeError = action.payload.errorMessage;
+          return;
+        }
+        state.loadResumeError = action.error.message;
       })
       // createCareer
       .addCase(createCareer.pending, (state) => {
         state.createCareerLoading = true;
-        state.createCareerError = null;
+        state.createCareerError = undefined;
       })
       .addCase(createCareer.fulfilled, (state, action) => {
         state.createCareerLoading = false;
         state.careers = action.payload;
-        state.createCareerError = null;
+        state.createCareerError = undefined;
       })
       .addCase(createCareer.rejected, (state, action) => {
         state.createCareerLoading = false;
-        state.createCareerError = action.payload;
+        if (action.payload) {
+          state.createCareerError = action.payload.errorMessage;
+          return;
+        }
+        state.createCareerError = action.error.message;
       })
       // deleteCareer
       .addCase(deleteCareer.pending, (state) => {
         state.deleteCareerLoading = true;
-        state.deleteCareerError = null;
+        state.deleteCareerError = undefined;
       })
       .addCase(deleteCareer.fulfilled, (state, action) => {
         state.deleteCareerLoading = false;
         state.careers = action.payload;
-        state.deleteCareerError = null;
+        state.deleteCareerError = undefined;
       })
       .addCase(deleteCareer.rejected, (state, action) => {
         state.deleteCareerLoading = false;
-        state.deleteCareerError = action.payload;
+        if (action.payload) {
+          state.createCareerError = action.payload.errorMessage;
+          return;
+        }
+        state.createCareerError = action.error.message;
       }),
 });
 
--- redux/thunks.ts
+++ redux/thunks.ts
@@ -1,3 +1,4 @@
+import { Career } from '@/types/Resume';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import {
@@ -6,36 +7,50 @@ import {
   updateResume,
   fetchResume,
 } from '@/services/resume';
+import { ResumeState } from './slice';
 
-export const loadResume = createAsyncThunk('resume/loadResume', async () => {
+interface ValidationErrors {
+  errorMessage: string;
+  // eslint-disable-next-line camelcase
+  field_errors: Record<string, string>;
+}
+
+export const loadResume = createAsyncThunk<
+  ResumeState,
+  void,
+  { rejectValue: ValidationErrors }
+>('resume/loadResume', async () => {
   return fetchResume()
     .then((res) => res.data)
     .catch((error) => error);
 });
 
-export const changeResume = createAsyncThunk(
-  'resume/changeResume',
-  async () => {
-    return updateResume()
-      .then((res) => res) // res.data로 나중에 변경 필요
-      .catch((error) => error);
-  }
-);
+export const changeResume = createAsyncThunk<
+  ResumeState,
+  void,
+  { rejectValue: ValidationErrors }
+>('resume/changeResume', async () => {
+  return updateResume()
+    .then((res) => res) // res.data로 나중에 변경 필요
+    .catch((error) => error);
+});
 
-export const createCareer = createAsyncThunk(
-  'resume/createCareer',
-  async () => {
-    return addCareer()
-      .then((res) => res.data)
-      .catch((error) => error);
-  }
-);
+export const createCareer = createAsyncThunk<
+  Career[],
+  void,
+  { rejectValue: ValidationErrors }
+>('resume/createCareer', async () => {
+  return addCareer()
+    .then((res) => res.data)
+    .catch((error) => error);
+});
 
-export const deleteCareer = createAsyncThunk(
-  'resume/deleteCareer',
-  async (id: string) => {
-    return removeCareer(id)
-      .then((res) => res.data)
-      .catch((error) => error);
-  }
-);
+export const deleteCareer = createAsyncThunk<
+  Career[],
+  string,
+  { rejectValue: ValidationErrors }
+>('resume/deleteCareer', async (id: string) => {
+  return removeCareer(id)
+    .then((res) => res.data)
+    .catch((error) => error);
+});
