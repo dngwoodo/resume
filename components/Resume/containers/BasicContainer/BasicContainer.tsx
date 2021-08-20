42d84b029adbacba6df1da5dfd2e0b11e00abf30
dngwoodo <dngwoodo@gmail.com>
2021-08-20 19:59:42 +0900

    refacor: Change basic name type
    basic name type을 좀 더 명시적으로 inputName<Basic>으로 변경

--- components/Resume/containers/BasicContainer/BasicContainer.tsx
+++ components/Resume/containers/BasicContainer/BasicContainer.tsx
@@ -2,6 +2,7 @@ import useAppDispatch from '@/hooks/useAppDispatch';
 import useAppSelector from '@/hooks/useAppSelector';
 
 import { changeBasicField } from '@/redux/slice';
+import { Basic as BasicType, InputName } from '@/types/Resume';
 
 import Basic from '../../components/Basic';
 
@@ -9,20 +10,26 @@ export default function BasicContainer() {
   const dispatch = useAppDispatch();
   const { basic } = useAppSelector((state) => state.resume);
 
-  function handleChange({ name, value }: { name: string; value: string }) {
+  function handleChange({
+    name,
+    value,
+  }: {
+    name: InputName<BasicType>;
+    value: string;
+  }) {
     dispatch(changeBasicField({ name, value }));
   }
 
-  const { name, occupation, email, phoneNumber, address, introduction } = basic;
+  const { name, jobTitle, email, phone, address, selfIntroduction } = basic;
 
   return (
     <Basic
       name={name}
-      occupation={occupation}
+      jobTitle={jobTitle}
       email={email}
-      phoneNumber={phoneNumber}
+      phone={phone}
       address={address}
-      introduction={introduction}
+      selfIntroduction={selfIntroduction}
       onChange={handleChange}
     />
   );
